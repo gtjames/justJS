24de8d9db497b9aded82f68e0c443602c048433a
Gary James <gtjames@gmail.com>
2021-05-09 09:29:40 -0500

    Update volcanoes.js

--- volcanoes.js
+++ volcanoes.js
@@ -14,19 +14,24 @@ fs.readFile('volcanoes.json', {
     volcanoes = JSON.parse(data);
     console.log(`There are #{volcanoes.length} volcanoes in our list`);
 
-    //  this will tidy up our data. The Deaths attribute is inside of quotes
-    //  so JS sees it as a string. This little tweeak will convert Deaths to a number
-    volcanoes.forEach(v => v.Deaths = +v.Deaths);
+    //  this will tidy up our data. The DEATHS attribute is inside of quotes
+    //  so JS sees it as a string. This little tweak will convert DEATHS to a number
+    volcanoes.forEach(v => v.DEATHS = +v.DEATHS);
 
-    let deaths = volcanoes.reduce((total, v) => total + v.Deaths, 0);
+    //  calculate the total number of deaths
+    let deaths = volcanoes.reduce((total, v) => total + v.DEATHS, 0);
     console.table(`number of deaths ${deaths}`);
     let aveDeaths = deaths / volcanoes.length;
-    console.table(`avegage of deaths ${aveDeaths.toFixed(0)}`);
+    console.table(`average number of deaths ${aveDeaths.toFixed(0)}`);
 
     let elevationTot = volcanoes.reduce((total, v) => total + v.Elevation, 0);
     let aveElevation = elevationTot / volcanoes.length;
-    console.table(`Total Elevation ${aveElevation.toFixed(0)}`);
+    console.table(`Average Elevation of all volcanoes ${aveElevation.toFixed(0)}`);
 
+    // use map to extract just the names of the volcanoes
     let names = volcanoes.map(v => v.Name);
     //console.table(names);
+
+    let newZealand = volcanoes.filter(v => v.Country === 'New Zealand');
+    console.log(`There were ${newZealand.length} volcanoes in New Zealand`);
 })
